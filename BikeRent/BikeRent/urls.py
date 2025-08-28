064a4e403e464cf11b95c92ea64d3acae9e1eebc
Aleksei <loguntsovae.teachmeskills@gmail.com>
2025-08-28 12:28:15 +0400

    Develop

--- BikeRent/BikeRent/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("bikes/", include("bike.urls")),
-    path("rents/", include("rent.urls")),
-]
